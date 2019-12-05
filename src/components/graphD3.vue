d8307b04912f049c7eb2a3392a21043eda5873a3
pongsatorn <ton11797@hotmail.com>
2019-12-05 18:19:54 +0700

    graph view

---
+++ src/components/graphD3.vue
@@ -0,0 +1,43 @@
+<template>
+    <div>
+        <d3-network @node-click='clickNode' :net-nodes="data.nodes" :net-links="data.links" :options="options" />
+    </div>
+</template>
+
+<script>
+import D3Network from 'vue-d3-network'
+export default {
+    name:"Chart",
+    props: ['data','Width'],
+    components: {
+      D3Network
+    },
+    data(){
+        return{
+          nodeSize:50,
+          canvas:false,
+          Width:0
+        }
+    },
+    computed:{
+      options(){
+        return{
+          force: 3000,
+          size:{ w:window.innerWidth-350, h:500},
+          nodeSize: this.nodeSize,
+          nodeLabels: true,
+          linkLabels:true,
+          canvas: this.canvas
+        }
+      }
+    },
+    methods: {
+      clickNode(e,n){
+        console.log(e)
+        console.log(n)
+      }
+    },
+}
+</script>
+
+<style src="vue-d3-network/dist/vue-d3-network.css"></style>
--- src/views/manage/version.vue
+++ src/views/manage/version.vue
@@ -1,12 +1,12 @@
 <template>
   <div>
-    <CCard>
+    <CCard ref="card">
       <CCardHeader>
-        <CIcon name="cil-applications"/> Version view
+        <CIcon name="cil-drop"/> Version view
       </CCardHeader>
       <CCardBody>
         <CRow>
-          
+          <graphD3 :data='data' :Width='600' />
         </CRow>
       </CCardBody>
     </CCard>
@@ -14,7 +14,47 @@
 </template>
 
 <script>
+import graphD3 from '@/components/graphD3'
 export default {
-  name: 'Colors'
+  name: 'VersionManagement',
+  components:{graphD3},
+  data(){
+    return{
+      data:{
+        Width:600,
+         nodes: [
+        { id: 1, name: 'my awesome node 1'},
+        { id: 2, name: 'my node 2'},
+        { id: 3, name:'orange node', _color: 'orange' },
+        { id: 4, _color: '#4466ff'},
+        { id: 5 },
+        { id: 6 },
+        { id: 7 },
+        { id: 8 },
+        { id: 9 }
+      ],
+      links: [
+        { sid: 1, tid: 2 },
+        { sid: 2, tid: 8 },
+        { sid: 3, tid: 4,  _svgAttrs:{'stroke-width':8,opacity:1},name:'custom link' },
+        { sid: 4, tid: 5 },
+        { sid: 5, tid: 6 },
+        { sid: 7, tid: 8 },
+        { sid: 5, tid: 8 },
+        { sid: 3, tid: 8 },
+        { sid: 7, tid: 9 }
+      ]
+      }
+    }
+  },
+  beforeMount(){
+
+  },
+  computed:{
+    
+  },
+  methods: {
+
+  }
 }
 </script>
