a8122b3481efe09fabf064142642311e378f30c8
Pongsatorn <ton11797@hotmail.com>
2020-03-30 17:09:51 +0700

    update

---
+++ src/views/manage/confirmation.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="top">
+      <CCard>
+      <CCardHeader>
+        <CIcon name="cil-drop" />Confirm data
+      </CCardHeader>
+      <CCardBody>
+
+      </CCardBody>
+    </CCard>
+  </div>
+</template>
+
+<script>
+import _ from "lodash";
+import api from "@/API";
+export default {
+  name: "SystemManagement",
+  data() {
+    return {
+    };
+  },
+  beforeMount() {
+    this.fetchData();
+  },
+  methods: {
+    async fetchData() {
+      try {
+        this.respond = (await api.system.getConfig()).data
+        console.log(respond)
+      } catch (error) {}
+      this.loading = false
+    }
+  }
+};
+</script>
